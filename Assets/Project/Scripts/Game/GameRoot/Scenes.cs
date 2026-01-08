d499f7bedef4d74f50102a31314aab83ea5ff3b9
B1g_B0$$ <grand0077@inbox.ru>
2026-01-08 22:29:42 +0300

    1 Часть кодовой базы

---
+++ Assets/Project/Scripts/Game/GameRoot/Scenes.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Scripts.Game.GameRoot
+{
+    public static class Scenes
+    {
+        public const string Boot = nameof(Boot);
+        public const string MainMenu = nameof(MainMenu);
+    }
+}
