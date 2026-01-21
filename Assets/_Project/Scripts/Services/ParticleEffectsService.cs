b3eb08584c2545b851f8f87fe23ef33492ae6cdd
B1g_B0$$ <grand0077@inbox.ru>
2026-01-21 20:04:22 +0300

    Пак с анимациями от полигона

--- Assets/_Project/Scripts/Services/ParticleEffectsService.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Project.Scripts.Services
-{
-    public class ParticleEffectsService : MonoBehaviour, IService
-    {
-        public bool IsInitiated { get; }
-    }
-}
--- Assets/_Project/Scripts/UI/View/ViewFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Project.Scripts.UI.View
-{
-    public class ViewFactory : MonoBehaviour
-    {
-        
-    }
-}
