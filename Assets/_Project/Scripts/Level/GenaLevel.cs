86d7631e5f0dd03761c48087fe8c33b39f9d66de
B1g_B0$$ <grand0077@inbox.ru>
2026-03-08 20:16:13 +0300

    Коммит настроеных врагов в сцене гены

---
+++ Assets/_Project/Scripts/Level/GenaLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _Project.Scripts.Level
+{
+    public class GenaLevel : Level
+    {
+        private void OnEnable()
+        {
+            IsInitiatedSpawners += SpawnStartWaves;
+        }
+
+        private void OnDisable()
+        {
+            IsInitiatedSpawners -= SpawnStartWaves;
+        }
+
+        private void SpawnStartWaves()
+        {
+            CreateWaveOfEnemies(FirstWaveEnemy);
+        }
+    }
+}
