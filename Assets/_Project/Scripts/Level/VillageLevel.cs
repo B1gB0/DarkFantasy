9f1468d40ad87d00d311dcdc1717d120800e3cb3
B1g_B0$$ <grand0077@inbox.ru>
2026-03-13 12:26:55 +0300

    Небольшой геймдизайн в деревне, небольшие изменения

---
+++ Assets/_Project/Scripts/Level/VillageLevel.cs
@@ -0,0 +1,20 @@
+﻿namespace _Project.Scripts.Level
+{
+    public class VillageLevel : Level
+    {
+        // private void OnEnable()
+        // {
+        //     IsInitiatedSpawners += SpawnStartWaves;
+        // }
+        //
+        // private void OnDisable()
+        // {
+        //     IsInitiatedSpawners -= SpawnStartWaves;
+        // }
+        //
+        // private void SpawnStartWaves()
+        // {
+        //     CreateWaveOfEnemies(FirstWaveEnemy);
+        // }
+    }
+}
