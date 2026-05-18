aac969763f9fb1c9c6dad6196e7920dcc6d4d817
B1g_B0$$ <grand0077@inbox.ru>
2026-05-18 09:46:58 +0300

    Дизайн локации для пролога

---
+++ Assets/_Project/Scripts/Level/PrologueLevel.cs
@@ -0,0 +1,54 @@
+﻿using _Project.Scripts.Level.Triggers;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _Project.Scripts.Level
+{
+    public class PrologueLevel : Level
+    {
+        [SerializeField] private NextLevelTrigger _nextLevelTrigger;
+        
+        private void OnEnable()
+        {
+            IsInitiatedSpawners += SpawnStartWaves;
+        }
+
+        private void OnDisable()
+        {
+            IsInitiatedSpawners -= SpawnStartWaves;
+        }
+
+        private void OnDestroy()
+        {
+            EnemySpawner.OnAllEnemiesKilled -= _nextLevelTrigger.Activate;
+            _nextLevelTrigger.OnGoToNextLevel -= HandleMissionTransition;
+        }
+
+        public override async UniTask OnStartLevel()
+        {
+            await base.OnStartLevel();
+            
+            EnemySpawner.OnAllEnemiesKilled += _nextLevelTrigger.Activate;
+            
+            _nextLevelTrigger.OnGoToNextLevel += HandleMissionTransition;
+        }
+
+        private void SpawnStartWaves()
+        {
+            CreateWaveOfEnemies(FirstWaveEnemy);
+            CreateWaveOfEnemies(SecondWaveEnemy);
+            CreateWaveOfEnemies(ThirdWaveEnemy);
+        }
+
+        private void SpawnLastWave()
+        {
+            CreateWaveOfEnemies(FourthWaveEnemy);
+        }
+        
+        private void HandleMissionTransition()
+        {
+            ViewFactory.GameplayEntryPoint.GetVillageHubExitParameters();
+            ViewFactory.UIScene.HandleGoToNextSceneButtonClick();
+        }
+    }
+}
