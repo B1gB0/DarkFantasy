af8e4e4e40f462835d7eb98e5013dfe74c9a9369
Grand <grand0077@inbox.ru>
2026-09-02 19:31:43 +0300

    Фикс анимаций у DarkLord теперь всё работает как задумано

--- Assets/_Project/Scripts/Weapon/Enemy/DarkLordAnimationEventsForWeapon.cs
+++ Assets/_Project/Scripts/Weapon/Enemy/DarkLordAnimationEventsForWeapon.cs
@@ -6,8 +6,10 @@ namespace _Project.Scripts.Weapon.Enemy
     {
         [SerializeField] private Coil _coil;
         [SerializeField] private FireballSpell _fireballSpell;
+        [SerializeField] private MeleeWeapon _meleeWeapon;
 
         public void OnCoilAttack() => _coil.Attack();
         public void FireballAttack() => _fireballSpell.Attack();
+        public void MeleeWeaponAttack() => _meleeWeapon.Attack();
     }
 }
